8356cb6a9cead4c8402c2cb5b3d1206eb536fabc
Pier Fumagalli <pier@usrz.com>
2025-10-01 15:28:05 +0200

    Build script

---
+++ build.sh
@@ -0,0 +1,106 @@
+#!/bin/bash -e
+
+rm -rf "${PWD}/dist"
+mkdir -p "${PWD}/dist"
+
+readonly BASEDIR="${PWD}"
+readonly PREFIX="${PWD}/dist"
+
+# ===== Environment Setup ======================================================
+
+# The PostgreSQL version to build
+readonly POSTGRESQL_VERSION=16.10
+
+# On macOS, target only Big Sur (11.0) and later (M1 support)
+readonly MACOSX_DEPLOYMENT_TARGET=11.0
+export MACOSX_DEPLOYMENT_TARGET
+
+# ===== Platform and Architecture ==============================================
+
+readonly NODE_PLATFORM="$(node -p 'process.platform')"
+readonly NODE_ARCH="$(node -p 'process.arch')"
+readonly NODE_OS="${NODE_PLATFORM}-${NODE_ARCH}"
+readonly NODE_INCLUDE_DIR="$(node -p 'path.resolve(process.execPath, "..", "..", "include", "node")')"
+
+# ===== OpenSSL Build ==========================================================
+
+# Figure out the OpenSSL version to build (the RE is to extract the "x.y.z" part
+# when NodeJS reports something like "3.0.15+quic").
+OPENSSL_VERSION="$(node -p 'process.versions.openssl.match(/\d+\.\d+\.\d+/)[0]')"
+
+# Download OpenSSL and expand it
+rm -rf "./openssl-${OPENSSL_VERSION}"
+curl -LO "https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz"
+tar -zxvf "./openssl-${OPENSSL_VERSION}.tar.gz"
+
+# OpenSSL build target
+case "${NODE_OS}" in
+  "darwin-arm64")
+    OPENSSL_TARGET="darwin64-arm64-cc"
+    ;;
+  "darwin-x64")
+    OPENSSL_TARGET="darwin64-x86_64-cc"
+    ;;
+  "linux-x64")
+    OPENSSL_TARGET="linux-x86_64"
+    ;;
+  "linux-arm64")
+    OPENSSL_TARGET="linux-aarch64"
+    ;;
+  *)
+    echo "Unsupported platform/architecture: ${NODE_OS}"
+    exit 1
+    ;;
+esac
+
+# Build OpenSSL and install the binaries/headers
+cd "./openssl-${OPENSSL_VERSION}"
+./Configure --prefix="${PREFIX}" \
+  --openssldir=/etc/ssl \
+  ${OPENSSL_TARGET}
+
+make
+make install_sw
+
+cd "${BASEDIR}"
+
+# ===== PostgreSQL Build =======================================================
+
+# Download PostgreSQL and expand it
+rm -rf "./postgresql-${POSTGRESQL_VERSION}"
+curl -LO "https://ftp.postgresql.org/pub/source/v${POSTGRESQL_VERSION}/postgresql-${POSTGRESQL_VERSION}.tar.gz"
+tar -zxvf "./postgresql-${POSTGRESQL_VERSION}.tar.gz"
+
+# Build PostgreSQL and install the binaries/headers for LibPQ only
+cd "./postgresql-${POSTGRESQL_VERSION}"
+LDFLAGS="-L${PREFIX}/lib" \
+./configure --prefix="${PREFIX}" \
+  --with-includes="${NODE_INCLUDE_DIR}" \
+  --enable-thread-safety \
+  --with-openssl \
+  --without-libxml \
+  --without-libxslt \
+  --without-python \
+  --without-readline \
+  --without-icu \
+  --without-lz4 \
+  --without-zstd
+
+make -C "./src/include"
+make -C "./src/common"
+make -C "./src/port"
+make -C "./src/interfaces/libpq"
+make -C "./src/bin/pg_config"
+
+make -C "./src/include" install
+make -C "./src/common" install
+make -C "./src/port" install
+make -C "./src/interfaces/libpq" install
+make -C "./src/bin/pg_config" install
+
+cd "${BASEDIR}"
+
+# ===== Final Node "libpq" build ===============================================
+
+./node_modules/.bin/node-gyp rebuild
+ls -la ./build/Release/libpq.node
